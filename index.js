0a94e61405fa7dcf397e2d4f4634033473b2d75a
Jessica Nelson <yahwehschica@yahoo.com>
2021-09-14 11:01:20 -0400

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,87 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push("Ralph")
+    return cats 
+}
+cats; 
+ 
+cats.length = 0;
+cats.push("Milo", "Otis", "Garfield");
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob")
+    return cats 
+} 
+cats; 
+
+cats.length = 0;
+cats.push("Milo", "Otis", "Garfield");
+
+function destructivelyRemoveLastCat() { 
+    cats.pop("Garfield") 
+    return cats 
+} 
+cats; 
+
+cats.length = 0;
+cats.push("Milo", "Otis", "Garfield");
+
+function destructivelyRemoveFirstCat() {
+    cats.shift("Milo")
+    return cats
+}
+cats; 
+
+cats.length = 0;
+cats.push("Milo", "Otis", "Garfield");
+
+function appendCat() {
+    appendCat = cats.concat("Broom")
+    return appendCat
+}
+appendCat;
+
+function prependCat() {
+    const prependCat= ["Arnold",...cats]
+    return prependCat
+   }
+   prependCat;
+
+   function removeLastCat() {
+       const removeLastCat = cats.slice(0,2)
+       return removeLastCat
+   }
+   removeLastCat;
+
+   function removeFirstCat() {
+        const removeFirstCat = cats.slice(1,)
+        return removeFirstCat
+   }
+   removeFirstCat;
+   
+
+   
+
+
+
+ 
+
+
+
+
+
+
+
+    
+
+ 
+    
+
+
+
+
+
+
+
+  
